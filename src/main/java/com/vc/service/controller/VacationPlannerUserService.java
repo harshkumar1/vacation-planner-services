5fd2a22ae2b78886fdfe7d59aefefca82e538966
Harsh Kumar <harshumar1@gmail.com>
2017-06-18 18:14:36 +0530

    Adding Notification Manager Backend with hard coded notifications

--- src/main/java/com/vc/service/controller/VacationPlannerUserService.java
+++ src/main/java/com/vc/service/controller/VacationPlannerUserService.java
@@ -1,4 +1,4 @@
-package com.vc.service;
+package com.vc.service.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
