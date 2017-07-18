a6d2d65bdbbcf64e1c99d7aa56c140ef33076d03
Harsh Kumar <harshkumar1@gmail.com>
2017-07-18 23:08:19 +0530

    Flight API Integration - Started using the QPX Client Library.
    https://developers.google.com/api-client-library/java/apis/qpxExpress/v1
    Currently assuming that the children are 0 and all guests are adults.
    By default searching for to-fro only
    No endpoint to search for one way travel

--- src/main/java/com/vc/service/controller/FlightSearchService.java
+++ src/main/java/com/vc/service/controller/FlightSearchService.java
@@ -1,102 +1,47 @@
 package com.vc.service.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.StringWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.SimpleDateFormat;
+import java.security.GeneralSecurityException;
 import java.util.Date;
-import java.util.Locale;
+import java.util.List;
+
 import javax.ws.rs.Produces;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vc.model.QPXExpressAirlineReservation;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.google.api.services.qpxExpress.model.TripOption;
 import com.vc.model.Trip;
+import com.vc.util.FlightFinder;
+import com.vc.util.GoogleQpxExpress;
 
 @RestController
 public class FlightSearchService {
 
-	private final String API_KEY="AIzaSyBdVHqzy39BzWHoyPQmRQR2ovR32hZuEy0";
-
 	@Produces(value = "application/json")
 	@RequestMapping(value="/flights", method=RequestMethod.POST)
 	public String getFlight(@RequestBody Trip tripModel) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd",Locale.US);
-
-		QPXExpressAirlineReservation airlineReservation = new QPXExpressAirlineReservation();
-		airlineReservation.setSolutions("10");
-
-		System.out.println( "Number of Guests: " + tripModel.getHotelInformation().getNumberOfGuests());
-		System.out.println( "Slice Length: " + airlineReservation.getSlice().length);
-		airlineReservation.getRequest().getPassengers().setAdultCount(tripModel.getHotelInformation().getNumberOfGuests());
-
-		Date date = new Date(Long.parseLong(tripModel.getStartDate()));
-		System.out.println("Date: " + sdf.format(date));
-		airlineReservation.getSlice()[0].setDate(sdf.format(date));
-		airlineReservation.getSlice()[0].setOrigin(tripModel.getOrigin().getAirportCode());
-		airlineReservation.getSlice()[0].setDestination(tripModel.getDestination().getAirportCode());
-
-		date = new Date(Long.parseLong(tripModel.getEndDate()));
-		airlineReservation.getSlice()[1].setDate(sdf.format(date));
-		airlineReservation.getSlice()[1].setOrigin(tripModel.getDestination().getAirportCode());
-		airlineReservation.getSlice()[1].setDestination(tripModel.getOrigin().getAirportCode());
-
-		String url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + API_KEY;
-		ObjectMapper objectMapper = new ObjectMapper();
-		StringWriter strFlight = new StringWriter();
-
-		URL obj = null;
-		HttpURLConnection con = null;
-		JSONArray  results = null;
-		OutputStream os = null;
-		
+		//SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd",Locale.US);
+		String jsonResponse = null;
+		//QPXExpressAirlineReservation airlineReservation = new QPXExpressAirlineReservation();
+		FlightFinder flightSearch = new GoogleQpxExpress();
+		Date departureDate = new Date (Long.parseLong(tripModel.getStartDate()));
+		Date returnDate = new Date (Long.parseLong(tripModel.getEndDate()));
 		try {
-			objectMapper.writeValue(strFlight, airlineReservation);
-			System.out.println(strFlight.toString());
-			obj = new URL(url);
-			con = (HttpURLConnection) obj.openConnection();
-			con.setRequestMethod("POST");
-			con.setRequestProperty("Content-Type","application/json");
-			con.setRequestProperty( "charset", "utf-8");
-			con.setRequestProperty( "Content-Length", Integer.toString( strFlight.toString().length() ));
-			os = con.getOutputStream();
-			os.write(strFlight.toString().getBytes());
-			os.flush();
-			os.close();
-			
-			int responseCode = con.getResponseCode();
-			if(responseCode == 200) {
-				BufferedReader br = new BufferedReader(new InputStreamReader(
-						(con.getInputStream())));
-				JSONParser parser = new JSONParser();
-				JSONObject jsonObj = (JSONObject) parser.parse(br);
-				results =  (JSONArray) jsonObj.get("results");
-			}
-		} catch ( IOException | ParseException e) {
+			List<TripOption> tripOptions = flightSearch.getTripResults(tripModel.getOrigin().getAirportCode(), tripModel.getDestination().getAirportCode(), departureDate, returnDate, Integer.parseInt(tripModel.getHotelInformation().getNumberOfGuests()), 0, 0);
+			ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+			jsonResponse = ow.writeValueAsString(tripOptions);
+			System.out.println("Trip Options: " + jsonResponse);
+		} catch (NumberFormatException | GeneralSecurityException | IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		finally {
-			con.disconnect();
-		}
 		
-		if (results != null)
-			System.out.println(results.toJSONString());
-		
-		return results.toJSONString();
-
+		return jsonResponse;
 	}
 
 }
